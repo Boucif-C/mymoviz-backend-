97672374555fd601c6d05cbe339aa2355d37dcbb
Boucif <chabani.boucif@gmail.com>
2022-11-16 14:13:16 +0100

    modify movies

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/movies',(req,res)=>{
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
     .then(response=> response.json())
     .then(data=>{
-        res.json({result:data.results});
+        res.json({movies:data.results});
     })
 
 })
